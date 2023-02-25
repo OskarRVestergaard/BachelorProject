3d3823bbe2f7b6bb62ad00dc39b1af421506ea49
Oskar <oskar@roegild.dk>
2023-02-25 21:44:49 +0100

    block impl?

--- Block/Block.go
+++ Block/Block.go
@@ -1,25 +1,36 @@
 package Block
 
 import (
+	"crypto/sha256"
 	"example.com/packages/models"
 	"time"
 )
 
 type Block struct {
-	Hash           string
-	PreviousHash   string
-	TrasactionsLog map[models.SignedTransaction]string
-	TimeStamp      time.Time
+	Hash            string
+	PreviousHash    string
+	TransactionsLog map[int]*models.SignedTransaction
+	//TimeStamp      time.Time
 }
 
-func (b *Block) MakeBlock() {
-	b.Hash = calculateHash()
-	b.TimeStamp = time.Now()
+func (b *Block) MakeBlock(transactions []*models.SignedTransaction, prevHash string) {
+	b.PreviousHash = prevHash
+	b.TransactionsLog = make(map[int]*models.SignedTransaction)
+	//b.TransactionsLog = transactions
+	b.Hash = b.calculateHash()
+	//b.calculateHash()
+	//b.TimeStamp = time.Now()
 
 }
 
-func calculateHash() string {
-	return cry
+func (b *Block) calculateHash() string {
+	h := sha256.New()
+	h.Write([]byte("hello world\n"))
+	h.Write([]byte(b.PreviousHash))
+	//calculatedHash := crypto.SHA256(b.PreviousHash)
+	//b.Hash = "asd"
+	//return h.Sum(nil)
+	return string(h.Sum(nil))
 }
 func main() {
 	now := time.Now()
