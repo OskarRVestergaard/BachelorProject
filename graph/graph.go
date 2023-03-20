53d717a965fda3ffab4a547eec44393eecd7f110
DrAguNovlIng <Daniel@p-m-a.dk>
2023-03-20 13:37:52 +0100

    added open functionality to merkletrees

--- graph/graph.go
+++ graph/graph.go
@@ -13,6 +13,12 @@ func main() {
 	fmt.Println(*dag)
 	merkle := CreateMerkleTree(*dag)
 	fmt.Println(*merkle)
+	fmt.Println("0")
+	fmt.Println(merkle.Open(0))
+	fmt.Println("3")
+	fmt.Println(merkle.Open(3))
+	fmt.Println("4")
+	fmt.Println(merkle.Open(4))
 }
 
 func newEmptyGraph(size int) *models.Graph {
@@ -92,4 +98,28 @@ func CreateMerkleTree(graph models.Graph) *MerkleTree {
 		tree.nodes[i] = hash_strategy.HashByteArray(toBeHashed)
 	}
 	return &tree
+}
+
+func (tree *MerkleTree) Open(openingIndex int) [][]byte {
+	if openingIndex < 0 {
+		panic("Index in merkle tree to open must not be negative!")
+	}
+	result := make([][]byte, 0, 1) //maybe instead of 1 choose math.Log(float64(len(tree.nodes))) (maximum size of nodes used in opening) THIS IS JUST AN OPTIMIZATION
+	var isEven bool
+	firstLeaf := len(tree.nodes) / 2
+	//some loop
+	i := openingIndex + firstLeaf
+	j := 0
+	for i > 0 {
+		isEven = (i-firstLeaf)%2 == 0
+		if isEven {
+			j = i + 1
+		} else {
+			j = i - 1
+		}
+		result = append(result, tree.nodes[j])
+		i = (i+1)/2 - 1 //Go to parent
+	}
+	return result
+
 }
