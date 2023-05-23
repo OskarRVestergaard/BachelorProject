0ee57f55f124dd214ce964c95f1e5fc79ef47480
DrAguNovlIng <Daniel@p-m-a.dk>
2023-05-23 16:06:58 +0200

    reworked tests, pow not working

--- test/blocktree_test.go
+++ test/blocktree_test.go
@@ -2,6 +2,7 @@ package test
 
 import (
 	"github.com/OskarRVestergaard/BachelorProject/production/strategies/peer_strategy"
+	"sync"
 	"testing"
 	"time"
 
@@ -25,12 +26,19 @@ func testBlockChain(t *testing.T, constants TestingConstants) {
 	noOfPeers := constants.noOfPeers
 	noOfMsgs := constants.noOfMsgs
 	listOfPeers, pkList := test_utils.SetupPeers(noOfPeers, noOfPeers, constants.useProofOfSpace, constants.peerConstants) //setup peer
+	var wg sync.WaitGroup
+	wg.Add(len(listOfPeers))
 	for _, peer := range listOfPeers {
-		err := peer.StartMining(constants.ProofSizeN)
-		if err != nil {
-			print(err.Error())
-		}
+		go func(p peer_strategy.PeerInterface) {
+			err := p.StartMining(constants.ProofSizeN)
+			if err != nil {
+				wg.Done()
+				panic(err.Error())
+			}
+			wg.Done()
+		}(peer)
 	}
+	wg.Wait()
 	time.Sleep(constants.peerConstants.SlotLength)
 	starTime := time.Now()
 	for _, peer := range listOfPeers {
@@ -40,12 +48,18 @@ func testBlockChain(t *testing.T, constants TestingConstants) {
 		test_utils.SendMsgs(noOfMsgs, noOfPeers, listOfPeers, pkList)
 		time.Sleep(constants.WaitBetweenMessage)
 	}
+	wg.Add(len(listOfPeers))
 	for _, peer := range listOfPeers {
-		err := peer.StopMining()
-		if err != nil {
-			print(err.Error())
-		}
+		go func(p peer_strategy.PeerInterface) {
+			err := p.StopMining()
+			if err != nil {
+				wg.Done()
+				panic(err.Error())
+			}
+			wg.Done()
+		}(peer)
 	}
+	wg.Wait()
 	time.Sleep(constants.peerConstants.SlotLength)
 	if constants.useProofOfSpace {
 		for i, _ := range listOfPeers {
@@ -80,175 +94,6 @@ func testBlockChain(t *testing.T, constants TestingConstants) {
 		print(&chainFromHead)
 		print(&visualTree)
 	}
-}
-
-func TestPOWNetwork2Peers(t *testing.T) {
-	noOfPeers := 2
-	noOfMsgs := 3
-	noOfNames := 2
-	constants := peer_strategy.GetStandardConstants()
-	listOfPeers, pkList := test_utils.SetupPeers(noOfPeers, noOfNames, false, constants) //setup peer
-	test_utils.SendMsgs(noOfMsgs, noOfPeers, listOfPeers, pkList)                        //send msg
-	for _, peer := range listOfPeers {
-		err := peer.StartMining(0)
-		if err != nil {
-			print(err.Error())
-		}
-	}
-	time.Sleep(5000 * time.Millisecond)
-	starTime := time.Now()
-	for _, peer := range listOfPeers {
-		peer.ActivatePeer(starTime, constants.SlotLength)
-	}
-	for i := 0; i < 10; i++ {
-		test_utils.SendMsgs(noOfMsgs, noOfPeers, listOfPeers, pkList)
-		time.Sleep(4000 * time.Millisecond)
-	}
-	for _, peer := range listOfPeers {
-		err := peer.StopMining()
-		if err != nil {
-			print(err.Error())
-		}
-	}
-	time.Sleep(7000 * time.Millisecond)
-	tree1 := (listOfPeers[0].GetBlockTree()).(PoWblockchain.Blocktree)
-	tree2 := listOfPeers[1].GetBlockTree().(PoWblockchain.Blocktree)
-	assert.True(t, tree1.Equals(tree2))
-}
-
-func TestPOWNetwork16Peers(t *testing.T) {
-	noOfPeers := 16
-	noOfMsgs := 2
-	noOfNames := 16
-	constants := peer_strategy.GetStandardConstants()
-	constants.Hardness = 30
-	listOfPeers, pkList := test_utils.SetupPeers(noOfPeers, noOfNames, false, constants) //setup peer
-	test_utils.SendMsgs(noOfMsgs, noOfPeers, listOfPeers, pkList)                        //send msg
-	for _, peer := range listOfPeers {
-		err := peer.StartMining(0)
-		if err != nil {
-			print(err.Error())
-		}
-	}
-	for i := 0; i < 8; i++ {
-		test_utils.SendMsgs(noOfMsgs, noOfPeers, listOfPeers, pkList)
-		time.Sleep(6000 * time.Millisecond)
-	}
-	for _, peer := range listOfPeers {
-		err := peer.StopMining()
-		if err != nil {
-			print(err.Error())
-		}
-	}
-	time.Sleep(10000 * time.Millisecond)
-	for i, _ := range listOfPeers {
-		if i != 0 {
-			tree1 := listOfPeers[i-1].GetBlockTree().(PoWblockchain.Blocktree)
-			tree2 := listOfPeers[i].GetBlockTree().(PoWblockchain.Blocktree)
-			test := tree1.Equals(tree2)
-			assert.True(t, test)
-		}
-	}
-	assert.True(t, true)
-	tree1 := listOfPeers[0].GetBlockTree().(PoWblockchain.Blocktree)
-	visualTree := tree1.RootToVisualNode()
-	chainFromHead := tree1.HeadToChain()
-	print(&chainFromHead)
-	print(&visualTree)
-}
-
-func TestPoSpaceNetwork4Peers(t *testing.T) {
-	noOfPeers := 4
-	noOfMsgs := 4
-	noOfNames := 4
-	sizeOfProofsN := 32
-	constants := peer_strategy.GetStandardConstants()
-	listOfPeers, pkList := test_utils.SetupPeers(noOfPeers, noOfNames, true, constants) //setup peer
-	for _, peer := range listOfPeers {
-		err := peer.StartMining(sizeOfProofsN)
-		if err != nil {
-			print(err.Error())
-		}
-	}
-	time.Sleep(5000 * time.Millisecond)
-	starTime := time.Now()
-	for _, peer := range listOfPeers {
-		peer.ActivatePeer(starTime, constants.SlotLength)
-	}
-	time.Sleep(7000 * time.Millisecond) //Wait such that the peers are aware of who is mining
-	for i := 0; i < 4; i++ {
-		test_utils.SendMsgs(noOfMsgs, noOfPeers, listOfPeers, pkList)
-		time.Sleep(6000 * time.Millisecond)
-	}
-	for _, peer := range listOfPeers {
-		err := peer.StopMining()
-		if err != nil {
-			print(err.Error())
-		}
-	}
-	time.Sleep(6000 * time.Millisecond)
-	for i, _ := range listOfPeers {
-		if i != 0 {
-			tree1 := listOfPeers[i-1].GetBlockTree().(SpaceMintBlockchain.Blocktree)
-			tree2 := listOfPeers[i].GetBlockTree().(SpaceMintBlockchain.Blocktree)
-			test := tree1.Equals(tree2)
-			assert.True(t, test)
-		}
-	}
-	assert.True(t, true)
-	tree1 := listOfPeers[0].GetBlockTree().(SpaceMintBlockchain.Blocktree)
-	visualTree := tree1.RootToVisualNode()
-	chainFromHead := tree1.HeadToChain()
-	print(&chainFromHead)
-	print(&visualTree)
-}
-
-func TestPoSpaceNetwork16Peers(t *testing.T) {
-	noOfPeers := 16
-	noOfMsgs := 2
-	noOfNames := 16
-	sizeOfProofsN := 512
-	constants := peer_strategy.GetStandardConstants()
-	listOfPeers, pkList := test_utils.SetupPeers(noOfPeers, noOfNames, true, constants) //setup peer
-	for _, peer := range listOfPeers {
-		err := peer.StartMining(sizeOfProofsN)
-		if err != nil {
-			print(err.Error())
-		}
-	}
-	time.Sleep(20000 * time.Millisecond)
-	starTime := time.Now()
-	for _, peer := range listOfPeers {
-		peer.ActivatePeer(starTime, constants.SlotLength)
-	}
-	time.Sleep(10000 * time.Millisecond) //Wait such that the peers are aware of who is mining
-	for i := 0; i < 7; i++ {
-		test_utils.SendMsgs(noOfMsgs, noOfPeers, listOfPeers, pkList)
-		time.Sleep(8000 * time.Millisecond)
-	}
-	for _, peer := range listOfPeers {
-		err := peer.StopMining()
-		if err != nil {
-			print(err.Error())
-		}
-	}
-	time.Sleep(25000 * time.Millisecond)
-	for i, _ := range listOfPeers {
-		if i != 0 {
-			tree1 := listOfPeers[i-1].GetBlockTree().(SpaceMintBlockchain.Blocktree)
-			tree2 := listOfPeers[i].GetBlockTree().(SpaceMintBlockchain.Blocktree)
-			test := tree1.Equals(tree2)
-			if !test {
-				assert.True(t, test) //This conditional is for debugging purposes
-			}
-		}
-	}
-	assert.True(t, true)
-	tree1 := listOfPeers[0].GetBlockTree().(SpaceMintBlockchain.Blocktree)
-	visualTree := tree1.RootToVisualNode()
-	chainFromHead := tree1.HeadToChain()
-	print(&chainFromHead)
-	print(&visualTree)
 }
 
 func TestSlowOver15MinBig8PeerTestPoW(t *testing.T) {
